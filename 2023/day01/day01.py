5f295015318c7268f6c744f3818d5fc32a5fb6fb
macuisdein <mac@macuisdein.com>
2023-12-02 17:32:52 -0500

    Ran black formatter

--- 2023/day01/day01.py
+++ 2023/day01/day01.py
@@ -3,83 +3,85 @@
 sum = 0
 digits = []
 
-input = open("input2.txt", 'r')
+input = open("input2.txt", "r")
 lines = input.readlines()
 
 numLines = len(lines)
 
-for x in range(0,numLines):
+for x in range(0, numLines):
     rowDigit = list(filter(str.isdigit, lines[x]))
     digits.append(rowDigit)
     firstDigit = rowDigit[0]
     lastDigit = rowDigit[-1]
-    rowSum = (firstDigit)+ (lastDigit)
+    rowSum = (firstDigit) + (lastDigit)
     sum = sum + int(rowSum)
     # print(f"Row {x} digits are: {rowDigit} with the first={firstDigit} and last={lastDigit}. Callibration value is {rowSum}")
 
 print(f"Part 1 sum is {sum}")
 
 # Part 2
-# This one was unclear on the instructions about re-use. nineight is 98. 
+# This one was unclear on the instructions about re-use. nineight is 98.
 # I first went down a string.replace() method, but it replaced out of sequence. So I went for a brute force
 # And then, once I figured out about the overlap, I just modified the brute force. So not pretty, but it works.
 
 part2sum = 0
+
 
 def convertNum(row):
     length = len(row)
     new_row = row
     x = 0
     while x < length:
-        if row[x] == 'o':
-            if row[x+1] == 'n':
-                if row[x+2] == 'e':
-                    new_row = row[0:x+1] + '1' + row[x+2:length]
-        elif row[x] == 't':
-            if row[x+1] == 'w':
-                if row[x+2] == 'o':
-                    new_row = row[0:x+1] + '2' + row[x+2:length]
-            elif row[x+1] == 'h':
-                if row[x+2] == 'r':
-                    if row[x+3] == 'e':
-                        if row[x+4] == 'e':
-                            new_row = row[0:x+1] + '3' + row[x+4:length]
-        elif row[x] == 'f':
-            if row[x+1] == 'o':
-                if row[x+2] == 'u':
-                    if row[x+3] == 'r':
-                        new_row = row[0:x+1] + '4' + row[x+3:length]
-            elif row[x+1] == 'i':
-                if row[x+2] == 'v':
-                    if row[x+3] == 'e':
-                        new_row = row[0:x+1] + '5' + row[x+3:length]
-        elif row[x] == 's':
-            if row[x+1] == 'i':
-                if row[x+2] == 'x':
-                    new_row = row[0:x+1] + '6' + row[x+2:length]
-            elif row[x+1] == 'e':
-                if row[x+2] == 'v':
-                    if row[x+3] == 'e':
-                        if row[x+4] == 'n':
-                            new_row = row[0:x+1] + '7' + row[x+4:length]
-        elif row[x] == 'e':
-            if row[x+1] == 'i':
-                if row[x+2] == 'g':
-                    if row[x+3] == 'h':
-                        if row[x+4] == 't':
-                            new_row = row[0:x+1] + '8' + row[x+4:length]
-        elif row[x] == 'n':
-            if row[x+1] == 'i':
-                if row[x+2] == 'n':
-                    if row[x+3] == 'e':
-                        new_row = row[0:x+1] + '9' + row[x+3:length]
+        if row[x] == "o":
+            if row[x + 1] == "n":
+                if row[x + 2] == "e":
+                    new_row = row[0 : x + 1] + "1" + row[x + 2 : length]
+        elif row[x] == "t":
+            if row[x + 1] == "w":
+                if row[x + 2] == "o":
+                    new_row = row[0 : x + 1] + "2" + row[x + 2 : length]
+            elif row[x + 1] == "h":
+                if row[x + 2] == "r":
+                    if row[x + 3] == "e":
+                        if row[x + 4] == "e":
+                            new_row = row[0 : x + 1] + "3" + row[x + 4 : length]
+        elif row[x] == "f":
+            if row[x + 1] == "o":
+                if row[x + 2] == "u":
+                    if row[x + 3] == "r":
+                        new_row = row[0 : x + 1] + "4" + row[x + 3 : length]
+            elif row[x + 1] == "i":
+                if row[x + 2] == "v":
+                    if row[x + 3] == "e":
+                        new_row = row[0 : x + 1] + "5" + row[x + 3 : length]
+        elif row[x] == "s":
+            if row[x + 1] == "i":
+                if row[x + 2] == "x":
+                    new_row = row[0 : x + 1] + "6" + row[x + 2 : length]
+            elif row[x + 1] == "e":
+                if row[x + 2] == "v":
+                    if row[x + 3] == "e":
+                        if row[x + 4] == "n":
+                            new_row = row[0 : x + 1] + "7" + row[x + 4 : length]
+        elif row[x] == "e":
+            if row[x + 1] == "i":
+                if row[x + 2] == "g":
+                    if row[x + 3] == "h":
+                        if row[x + 4] == "t":
+                            new_row = row[0 : x + 1] + "8" + row[x + 4 : length]
+        elif row[x] == "n":
+            if row[x + 1] == "i":
+                if row[x + 2] == "n":
+                    if row[x + 3] == "e":
+                        new_row = row[0 : x + 1] + "9" + row[x + 3 : length]
         x = x + 1
         row = new_row
         length = len(row)
 
     return new_row
 
-for x in range(0,numLines):
+
+for x in range(0, numLines):
     row = lines[x]
     # print(f"Row {x}: {row}")
     row = convertNum(row)
@@ -88,7 +90,7 @@ for x in range(0,numLines):
     digits.append(rowDigit)
     firstDigit = rowDigit[0]
     lastDigit = rowDigit[-1]
-    rowSum = (firstDigit)+ (lastDigit)
+    rowSum = (firstDigit) + (lastDigit)
     part2sum = part2sum + int(rowSum)
     # print(f"Row {x} digits are: {rowDigit} with the first={firstDigit} and last={lastDigit}. Callibration value is {rowSum}")
     # print("\n")
